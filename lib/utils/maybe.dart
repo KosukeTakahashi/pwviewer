18d220ccf6a17b127cac2cf385e8894804dbcca0
KosukeTakahashi <kilotango37@icloud.com>
2021-05-15 16:59:10 +0900

    maybe class

---
+++ lib/utils/maybe.dart
@@ -0,0 +1,31 @@
+class Maybe<T> {
+  T? value;
+
+  Maybe.some(T? value) {
+    if (value == null) {
+      Maybe.nothing();
+    } else {
+      this.value = value;
+    }
+  }
+
+  Maybe.nothing() : value = null;
+
+  T unwrap() {
+    if (this.value == null) {
+      throw StateError('this.value is null');
+    } else {
+      return this.value!;
+    }
+  }
+
+  T? unwrapOrNull() {
+    return this.value;
+  }
+
+  Maybe<U> map<U>(U Function(T) projection) {
+    return this.value == null
+        ? Maybe<U>.nothing()
+        : Maybe<U>.some(projection(this.value!));
+  }
+}
